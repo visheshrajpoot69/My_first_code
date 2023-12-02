d0b7f62446a7796bbd937772b469f874adad6b51
rajpootvishesh6 <29949670-rajpootvishesh6@users.noreply.replit.com>
2023-12-02 21:17:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let vishesh = "ata";
+var surjeet = "chana";
+const ragni = "alubhujia";
+if (true) {
+const shibam = "chutiya";
+  let vishesh = "tamatar";
+  vishesh = "hello";
+console.log(vishesh);
+}
+console.log(vishesh);
+
+
+
