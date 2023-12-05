b45b0332a22d143de84bc1ef53113b34582c955a
rajpootvishesh6 <29949670-rajpootvishesh6@users.noreply.replit.com>
2023-12-05 18:21:38 +0000

    First file

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-let vishesh = "ata";
-var surjeet = "chana";
-const ragni = "alubhujia";
-if (true) {
-const shibam = "chutiya";
-  let vishesh = "tamatar";
-  vishesh = "hello";
-console.log(vishesh);
+let gojo = "567",sukuna = "789";
+/** 
+java script:-
+
+  1) primitive data types:- st-stack
+    **a) number let a = 95; 
+    **b) string let b = '23'
+    *c) boolean let c = true;
+    *d) undefined let d = undefined
+    *e) null let e = null;
+    f) symbol let f = Symbol('hello');
+    let h = Symbol('hello');
+    g) bigint let g = BigInt('567');
+  2) non primitive data types:- st-heap
+    **a) object let obj = {
+    'name' : 'goku',
+    'age' : 23,
+    'address' : 'd ball z',
+    phone : 95846265,
+    book : {
+    name : 'harry potter',
+    page : 65
+    },
+  array : ['goku', 'vegeta', 'gohan']
+  
+    }
+    **b) array let array = [
+    'goku',
+    'gohan',
+    'luffy'
+    ]
+    
+    **c) function 
+*/
+let obj = {
+  0 : 'goku',
+  1 : 'gohan',
+  2 : 'luffy'
 }
-console.log(vishesh);
+let array = [
+  'goku',
+  'gohan',
+  'luffy'
 
-
-
+]
